a4fcbb9ffde23699751a9808d8b09591cbbde7e7
TageZ <tagezerby@gmail.com>
2024-05-24 22:56:00 -0500

    Added info grid for experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,7 +7,16 @@ function Experience(){
                 <span className='title'>Experience</span>
                 <div className='experience-grid'>
                     <div className='experience-info'>
-                        
+                        Software Engineering Intern - Principal Financial Group
+                    </div>
+                    <div className='experience-info'>
+                        Software Development Intern - Hudl Design Studio Team
+                    </div>
+                    <div className='experience-info'>
+                        Computer Science Teaching Assistant - Raikes School
+                    </div>
+                    <div className='experience-info'>
+                        IT Intern - Collins Engineers
                     </div>
                 </div>
             </div>
