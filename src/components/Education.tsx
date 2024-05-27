b0a05b010e6f9a17f17ac93e2e0e6af7d7ebde37
TageZ <tagezerby@gmail.com>
2024-05-26 19:13:00 -0500

    Tweaks to sizing, start of education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -2,7 +2,19 @@
 
 function Education(){
     return (
-        <div>asdf</div>
+        <div className='portfolio-section' aria-label='education'>
+            <div className='education'>
+                <span className='title'>Education</span>
+                <div className='education-grid'>
+                    <div className='education-info'>
+                        
+                    </div>
+                    <div className='education-info'>
+                        Saint Charles North High School
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
