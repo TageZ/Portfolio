4f9c4825ba4da90f47650be5e1957b21e3b5cd02
TageZ <tagezerby@gmail.com>
2024-05-26 22:30:56 -0500

    Basic Projects page outline

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,24 @@
 function Projects(){
     return (
-        <div></div>
+        <div className='portfolio-section' aria-label='projects'>
+            <div className='projects'>
+                <span className='title'>Projects</span>
+                <div className='grid'>
+                    <div className='grid-item'>
+                        TBA
+                    </div>
+                    <div className='grid-item'>
+                        Pegs & Jokers
+                    </div>
+                    <div className='grid-item'>
+                        Premier League Resource
+                    </div>
+                    <div className='grid-item'>
+                        Portfolio Website
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
