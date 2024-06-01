1ee55b17aad00056d441a37cb875d4809c4c2065
TageZ <tagezerby@gmail.com>
2024-05-31 19:01:16 -0500

    Basic experience buttons made, css needs a lot of polishing

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,11 @@
 import '../styling/Grid.scss'
 import principal from '../assets/principal.png'
-import hudl from '../assets/hudl.jpg'
+import lincoln from '../assets/lincoln.jpg'
+import desmoines from '../assets/des-moines.jpg'
+import kauffman from '../assets/kauffman.jpg'
+import chicago from '../assets/chicago.jpg'
+import hudl from '../assets/hudl.png'
+import raikes from '../assets/raikes-school.png'
 import collins from '../assets/collins.png'
 
 function Experience(){
@@ -12,18 +17,22 @@ function Experience(){
             <div className='experience'>
                 <div className='grid'>
                     <div className='grid-item'>
-                        <img className='projects-image' src={principal}></img>
+                        <img className='projects-image' src={desmoines}></img>
+                        <img className='company-image' src={principal}></img>
                         {/* Software Engineering Intern - Principal Financial Group */}
                     </div>
                     <div className='grid-item'>
-                        <img className='projects-image' src={hudl}></img>
+                        <img className='projects-image' src={lincoln}></img>
+                        <img className='company-image' src={hudl}></img>
                         {/* Software Development Intern - Hudl - Design Studio */}
                     </div>
                     <div className='grid-item'>
-                        Computer Science Teaching Assistant - Raikes School
+                        <img className='projects-image' src={kauffman}></img>
+                        <img className='company-image' src={raikes}></img>
                     </div>
                     <div className='grid-item'>
-                        <img className='projects-image' src={collins}></img>
+                        <img className='projects-image' src={chicago}></img>
+                        <img className='company-image' src={collins}></img>
                         {/* IT Intern - Collins Engineers */}
                     </div>
                 </div>
